845805967e74e419cc4a1e14de9f21cb1a210e43
lakshmi1102-tech <konankilakshmi0211@gmail.com>
2024-12-07 15:54:13 +0530

    operators-1

---
+++ day3js.js
@@ -0,0 +1,48 @@
+var a=20;
+var a=30;
+ a+=200;
+console.log(a);
+var b=30;
+ b-=330;
+ console.log(b);
+
+ var a=10;
+ var b=20;
+ console.log(b>a&&a<b);
+ var a=10;
+ var b=20;
+ console.log(a&&b);
+
+ var a=10;
+ var b=20;
+ console.log(a<b||a>b);
+
+ var a=10;
+ console.log(!a);
+ var a=10;
+ console.log(!!a);
+
+  if(!!"lucky"){
+    console.log("true")
+  }else{
+    console.log("false")
+  }
+
+
+
+if(undefined&&undefined){
+    alert("hello there good morning");
+}else{
+   alert("hello!see you tomorrow");
+}  
+if("lucky"&&undefined){
+    alert("hello there good morning");
+}else{
+   alert("hello!see you tomorrow");
+}  
+ 
+if("lucky"||undefined){
+    alert("hello there good morning");
+}else{
+   alert("hello!see you tomorrow");
+}  
